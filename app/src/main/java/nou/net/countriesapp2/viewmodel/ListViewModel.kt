010e296fb3555f033540844aab8f7c701a85dd5c
Nolverto Urias Obeso <nolvertou@gmail.com>
2024-01-06 01:56:22 -0700

    Implementing the ViewModel

--- app/src/main/java/nou/net/countriesapp2/viewmodel/ListViewModel.kt
+++ app/src/main/java/nou/net/countriesapp2/viewmodel/ListViewModel.kt
@@ -1,7 +1,37 @@
 package nou.net.countriesapp2.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import nou.net.countriesapp2.Country
 
 class ListViewModel: ViewModel() {
 
+    // MutableLiveData
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    // Methods
+    fun refresh(){
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI"),
+            Country("CountryJ")
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
