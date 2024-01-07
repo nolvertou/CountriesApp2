9530128a79be8605f6935831d74b89fdb9a586f5
Nolverto Urias Obeso <nolvertou@gmail.com>
2024-01-07 01:26:06 -0700

    Implement the view

--- app/src/main/java/nou/net/countriesapp2/view/MainActivity.kt
+++ app/src/main/java/nou/net/countriesapp2/view/MainActivity.kt
@@ -2,11 +2,59 @@ package nou.net.countriesapp2.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import nou.net.countriesapp2.R
+import nou.net.countriesapp2.databinding.ActivityMainBinding
+import nou.net.countriesapp2.viewmodel.ListViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: ListViewModel
+    private val countriesAdapter = CountryListAdapter(arrayListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.countriesList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = countriesAdapter
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.countries.observe(this, Observer { countries ->
+            countries?.let{
+                countriesAdapter.updateCountries(it)
+            }
+        })
+
+        viewModel.countryLoadError.observe(this, Observer { isError ->
+            isError?.let{
+                binding.listError.visibility = if(it) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let{
+                binding.loadingView.visibility = if(it) View.VISIBLE else View.GONE
+                if(it){
+                    binding.listError.visibility = View.GONE
+                    binding.countriesList.visibility = View.GONE
+                }
+            }
+        })
     }
 }
