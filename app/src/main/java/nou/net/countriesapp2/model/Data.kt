ddb35e2b356220d6bdc2c3791c0b63f75c07cf05
Nolverto Urias Obeso <nolvertou@gmail.com>
2024-01-07 22:43:14 -0700

    Map the countryName using SerializedName and also add more variables

--- app/src/main/java/nou/net/countriesapp2/model/Data.kt
+++ app/src/main/java/nou/net/countriesapp2/model/Data.kt
@@ -1,5 +1,15 @@
 package nou.net.countriesapp2
 
+import com.google.gson.annotations.SerializedName
+
 data class Country(
-    val countryName: String?
+    /**
+     * We need a way to map the name we have in json to countryName
+     */
+    @SerializedName("name")
+    val countryName: String?,
+    @SerializedName("capital")
+    val capital: String?,
+    @SerializedName("flagPNG")
+    val flag: String?
 )
